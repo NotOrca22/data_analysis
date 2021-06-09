3a523cd08b7c1883c1517b8f6b364146535ecec4
git <547066921@qq.com>
2021-06-09 19:40:49 -0400

    turned it into 1 file

--- duplicates.py
+++ duplicates.py
@@ -1,4 +1,38 @@
+import bs4 as BeautifulSoup
+import mechanize
+
+import requests
 import argparse
+def removeDuplicates(input_file, gene_file, result_file):
+    with open(input_file) as f:
+        line = f.readline()
+        # print(line)
+        namesFound = []
+        indexes = []
+        count = 1
+        while line:
+            line = f.readline()
+            arr = line.split(",")
+            # print(arr)
+            if len(arr) > 1:
+                if arr[2] not in namesFound:
+                    namesFound.append(arr[2])
+                    indexes.append(count)
+                count += 1
+        # print(indexes)
+        br = mechanize.Browser()
+        br.open("https://www.biotools.fr/human/refseq_symbol_converter")
+        br.select_form(nr=0)
+        print(br.form)
+        br.form['input_data'] = "\n".join(namesFound)
+        print(br.form)
+        req = br.submit()
+        res = str(req.read())
+        res = res.split("\\n")
+        res[0] = res[0][2:]
+        print(res[0])
+        with open(gene_file, "w") as f:
+            f.write("\n".join(res[:len(namesFound)]))
 def replaceGenes(input_file, gene_file, result_file):
     text = []
     with open(input_file) as file:
@@ -30,10 +64,16 @@ def replaceGenes(input_file, gene_file, result_file):
         with open(result_file, "w") as f:
             for line in text:
                 f.write("\t".join(line))
-if __name__ == "__main__":
+
+
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_file", help="add the input file", action="store", dest="input_file")
+    # parser.add_argument("--output_file", help="add the output file", action="store", dest="output_file")
     parser.add_argument("--gene_file", help="add the gene file", action="store", dest="gene_file")
     parser.add_argument("--result_file", help="add the result file", action="store", dest="result_file")
     args = parser.parse_args()
+    removeDuplicates(args.input_file, args.gene_file, args.result_file)
     replaceGenes(args.input_file, args.gene_file, args.result_file)
